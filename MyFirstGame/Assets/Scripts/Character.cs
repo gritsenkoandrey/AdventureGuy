6eef95224480b3fb63bca8691bdd77d741b0204c
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-03-31 14:58:39 +0300

    Сделал передвижение и прыжок персонажа

--- MyFirstGame/Assets/Scripts/Character.cs
+++ MyFirstGame/Assets/Scripts/Character.cs
@@ -1,14 +1,37 @@
 ﻿using UnityEngine;
 
+
 public class Character : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private float _speed = 3;
+    [SerializeField] private int _health = 5;
+    [SerializeField] private float _jumpForce = 15;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetButton("Horizontal")) Run();
+        if(Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position,
+                transform.position + direction, _speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
