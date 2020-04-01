7ef41baffee79396c2a2dce4812d831380d31dac
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-04-01 13:01:55 +0300

    Добавил проверку на нахождение на земле и анимацию

--- MyFirstGame/Assets/Scripts/Character.cs
+++ MyFirstGame/Assets/Scripts/Character.cs
@@ -5,10 +5,18 @@ public class Character : MonoBehaviour
 {
     [SerializeField] private float _speed = 3;
     [SerializeField] private int _health = 5;
-    [SerializeField] private float _jumpForce = 15;
+    [SerializeField] private float _jumpForce = 10.0F;
+    private bool _isGround = false;
+
     private Rigidbody2D _rigidbody;
     private Animator _animator;
     private SpriteRenderer _sprite;
+
+    private CharacterState State
+    {
+        get { return (CharacterState)_animator.GetInteger("State"); }
+        set { _animator.SetInteger("State", (int)value);}
+    }
 
     private void Awake()
     {
@@ -17,10 +25,17 @@ public class Character : MonoBehaviour
         _sprite = GetComponent<SpriteRenderer>();
     }
 
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
     private void Update()
     {
+        if (_isGround) State = CharacterState.Idle;
+
         if(Input.GetButton("Horizontal")) Run();
-        if(Input.GetButtonDown("Jump")) Jump();
+        if(_isGround && Input.GetButtonDown("Jump")) Jump();
     }
 
     private void Run()
@@ -28,10 +43,29 @@ public class Character : MonoBehaviour
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position,
                 transform.position + direction, _speed * Time.deltaTime);
+
+        _sprite.flipX = direction.x < 0;
+
+        if (_isGround) State = CharacterState.Run;
     }
 
     private void Jump()
     {
         _rigidbody.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
     }
+
+    private void CheckGround()
+    {
+        var colliders = Physics2D.OverlapCircleAll(transform.position, 0.8F);
+        _isGround = colliders.Length > 1;
+
+        if (!_isGround) State = CharacterState.Jump;
+    }
+}
+
+public enum CharacterState
+{
+    Idle = 0,
+    Run = 1,
+    Jump = 2
 }
