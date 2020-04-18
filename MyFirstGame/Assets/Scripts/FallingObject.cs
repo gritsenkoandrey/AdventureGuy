4a4be759137648039c21ef43caf0e9bc8a2e1dbc
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-04-18 16:36:33 +0300

    Добавил падающие объекты через триггер

---
+++ MyFirstGame/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+public class FallingObject : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Character _character;
+
+    [SerializeField] private float _lifeTimeObject = 1.0f;
+    [SerializeField] private float _lifeTimeObjectAfterCollision = 0.2f;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    // при пересечении линии, Kinematic меняется на Dynamic, и на объект начинает действовать гравитация
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _character = collision.GetComponent<Character>();
+        if (_character)
+        {
+            _rigidbody.isKinematic = false;
+            Destroy(gameObject, _lifeTimeObject);
+        }
+
+        //if (collision.gameObject.tag.Equals("Player"))
+        //{
+        //    _rigidbody.isKinematic = false;
+        //}
+    }
+
+    // поведение объекта после падения
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _character = collision.collider.GetComponent<Character>();
+        if (_character)
+        {
+            _character.ReceiveDamage();
+            Destroy(gameObject, _lifeTimeObjectAfterCollision);
+        }
+    }
+}
